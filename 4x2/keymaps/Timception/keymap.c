0381749dfbe43f28153f3518d16674bc7ebc0047
Timothy Ryan <84595044+Timception@users.noreply.github.com>
2023-01-30 01:07:44 +0700

    Add files via upload

---
+++ 4x2/keymaps/Timception/keymap.c
@@ -0,0 +1,8 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+	[0] = LAYOUT_horizontal(
+		KC_A , KC_B , KC_C ,
+        KC_D , KC_E , KC_F 
+    )
+};
---
+++ 4x2/keymaps/default/readme.md
@@ -0,0 +1,3 @@
+# The default 4by2 keymap
+
+![The default 4by2 keymap](https://i.imgur.com/E4OlQAs.png)
